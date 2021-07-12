c487c5eafc40cc4c78a24d1a9f644ea1be6aef0f
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-12 15:46:38 -0400

    Initial Commit

---
+++ ColLaTeX/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
