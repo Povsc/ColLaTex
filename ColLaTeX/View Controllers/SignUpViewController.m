edf7e770ab0290c8e73f01babb2ebf676f257b98
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-13 11:29:57 -0400

    Implemented initial sign up

---
+++ ColLaTeX/View Controllers/SignUpViewController.m
@@ -0,0 +1,57 @@
+//
+//  SignUpViewController.m
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/13/21.
+//
+
+#import "SignUpViewController.h"
+@import Parse;
+
+@interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet PFImageView *imageView;
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)didTapRegister:(id)sender {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.email = self.emailField.text;
+    newUser.password = self.passwordField.text;
+        
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"toHomeScreen" sender:nil];
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
