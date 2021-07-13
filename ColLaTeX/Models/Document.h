2c6205816e99e936827c128f51e084ef7343379e
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-13 11:13:50 -0400

    Implemented Document data class

---
+++ ColLaTeX/Models/Document.h
@@ -0,0 +1,22 @@
+//
+//  Document.h
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/13/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface Document : PFObject<PFSubclassing>
+
+@property (nonatomic, strong) NSString *docID;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) PFUser *owner;
+@property (nonatomic, strong) NSString *content;
+@property (nonatomic, strong) NSMutableArray <PFFileObject *> *attachments;
+@property (nonatomic, strong) NSMutableArray <PFUser *> *sharedWith;
+
++ (void) newDocumentNamed: (NSString *)name withUsersArray: ( NSArray <PFUser *> *) arrayOfUsers withCompletion: (PFBooleanResultBlock  _Nullable)completion;
+
+@end
---
+++ ColLaTeX/Models/Document.m
@@ -0,0 +1,71 @@
+//
+//  Document.m
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/13/21.
+//
+
+#import "Document.h"
+
+@implementation Document
+    
+@dynamic docID;
+@dynamic owner;
+@dynamic content;
+@dynamic name;
+@dynamic attachments;
+@dynamic sharedWith;
+
++ (nonnull NSString *)parseClassName {
+    return @"Document";
+}
+
++ (void) newDocumentNamed: (NSString *)name withUsersArray: ( NSMutableArray <PFUser *> *) arrayOfUsers withCompletion: (PFBooleanResultBlock  _Nullable)completion {
+    
+    // Set document metadata
+    Document *newDoc = [Document new];
+    newDoc.name = name;
+    newDoc.owner = [PFUser currentUser];
+    newDoc.sharedWith = arrayOfUsers;
+    
+    // Create an empty document with title
+    NSString *beginning = [NSString stringWithFormat: @"\\documentclass[12pt]{article}\n"
+                           "\n"
+                           "\\usepackage[utf8]{inputenc}\n"
+                           "\\usepackage[spanish,activeacute]{babel}\n"
+                           "\n"
+                           "\\title{%@}\n", name];
+    NSString *end = [NSString stringWithFormat: @"\\author{%@}\n"
+    "\n"
+    "\\begin{document}\n"
+    "\n"
+    "\\maketitle\n"
+    "\n"
+    "\n"
+    "\n"
+    "\\end{document}", PFUser.currentUser.username];
+    
+    // Set content and attachments
+    newDoc.content = [beginning stringByAppendingFormat: @"%@", end];
+    newDoc.attachments = [NSMutableArray new];
+    
+    [newDoc saveInBackgroundWithBlock: completion];
+}
+
++ (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+ 
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    
+    return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+}
+
+@end
