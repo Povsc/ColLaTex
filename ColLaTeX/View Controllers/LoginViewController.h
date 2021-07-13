933dc8b63a884fb336369de07cde8db7be6cabb9
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-13 12:30:31 -0400

    User can log in

---
+++ ColLaTeX/View Controllers/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/13/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ColLaTeX/View Controllers/LoginViewController.m
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.m
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/13/21.
+//
+
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapLogin:(id)sender {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    // Attempt to log in
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"toHomeScreen" sender:nil];
+        }
+    }];
+}
+
+- (IBAction)didTapScreen:(id)sender {
+    [self.view endEditing:true];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
