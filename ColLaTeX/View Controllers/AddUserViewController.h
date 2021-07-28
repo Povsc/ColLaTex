53a04d1ab063bfdf8efdc54eafea51adfa9a2585
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-28 11:33:22 -0400

    Moved user search/autocomplete to a different modal view and reconfigured all else

---
+++ ColLaTeX/View Controllers/AddUserViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddUserViewController.h
+//  ColLaTeX
+//
+//  Created by felipeccm on 7/28/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddUserViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
